5ec433cb551df8d76680165ed1218261e25219f9
Thao Trinh <thaottb99@gmail.com>
2023-10-02 20:58:07 +0700

    Điều khiển xe sử dụng InputManager
    Quiz 1

---
+++ Buổi 3/Assets/Lession 3/Obj_Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_Move : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public Rigidbody rb;
+    Vector3 rotationRight = new Vector3(0, 30, 0);
+    Vector3 rotationLeft = new Vector3(0, -30, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationRight);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationLeft);
+        }
+    }
+}
